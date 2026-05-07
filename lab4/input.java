136ef29404ec0b362f95c211a1e2f10756cecd68
unknown <davidkagricenok@gmail.com>
2026-05-08 01:38:13 +0300

    'q'

---
+++ lab4/input.java
@@ -0,0 +1,11 @@
+public class Example {
+    /* This is a main block comment */
+    public static void main(String[] args) {
+        int count = 10;
+        String text = "Hello world!";
+        // Coment s is ts TypeScript
+        if (count > 2) {
+            System.out.println(text);
+        }
+    }
+}
---
+++ lab4/main.ts
@@ -0,0 +1,44 @@
+// Лабораторна роборта №3, Варіант №8
+/// <reference types="node" />
+import * as fs from "fs";
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function ask(question: string): Promise<string> {
+  return new Promise((resolve) => rl.question(question, resolve));
+}
+
+function transformLine(line: string): string {
+  return line.replace(/\b\w+\b/g, (word) =>
+    word.length > 2 ? word.toUpperCase() : word
+  );
+}
+
+async function main() {
+  const inputPath = (await ask("Введіть шлях до вхідного файлу: ")).trim();
+  const outputPath = (await ask("Введіть шлях до вихідного файлу: ")).trim();
+
+  console.log("Відкриття файлу:", inputPath);
+
+  try {
+    const data = fs.readFileSync(inputPath, "utf8");
+    console.log("Файл прочитано, обробка...");
+
+    const lines = data.split(/\r?\n/);
+    const outLines = lines.map(transformLine);
+
+    fs.writeFileSync(outputPath, outLines.join("\n"), "utf8");
+
+    console.log("Готово. Результат записано у файл:", outputPath);
+  } catch (err: any) {
+    console.error("Помилка:", err.message);
+  } finally {
+    rl.close();
+  }
+}
+
+main();
